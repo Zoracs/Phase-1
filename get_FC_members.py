52ca3266ab6abdd83402073b8eb40e1a36c99f11
Zoracs <regis24try@gmail.com>
2020-10-31 17:54:27 +0100

    Added test to get_FC_members

--- get_FC_members.py
+++ get_FC_members.py
@@ -6,8 +6,11 @@ def get_FC_members():
     except:
         print("XIV.API is currently not responding.")
     sortedList = []
+    print(str(r))
     for x in r.json()["FreeCompanyMembers"]:
         sortedList.append(x["Name"])
     for name in sortedList:
         print(name)
     return sortedList
+
+get_FC_members()

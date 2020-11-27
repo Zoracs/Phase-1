ff8e79cdf10b84ca8bd4b28261d8591300996cef
Zoracs <regis24try@gmail.com>
2020-11-27 15:39:13 +0100

    added exception for empty list and lints

--- CSV.py
+++ CSV.py
@@ -4,15 +4,17 @@
 def CSV_list(Username_list, Filename):
     File = open(f"{Filename}.csv", "w")
     lastEntry = len(Username_list) - 1
-
-    for x in Username_list:
-        if not Username_list:
+    if not Username_list:
             return "List is empty"
-        if x == Username_list[lastEntry]:
-            File.write(x)
-        # elif len(Username_list) == 0:
-        #     raise ValueError("List is empty.")
-        else:
-            File.write(f"{x}, ")
+    else:
+        for x in Username_list:
+            # if not Username_list:
+            #     return "List is empty"
+            if x == Username_list[lastEntry]:
+                File.write(x)
+            # elif len(Username_list) == 0:
+            #     raise ValueError("List is empty.")
+            else:
+                File.write(f"{x}, ")
     File.close()
     return f"{Filename}.csv"

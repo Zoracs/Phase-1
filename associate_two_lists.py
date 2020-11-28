26726f680150dfa9c29fb31a28e9fbccaefc3e96
dominic-byrne-worldpay <dominic.byrne@worldpay.com>
2020-11-28 11:46:11 +0000

    Added csv reader function and tests

---
+++ associate_two_lists.py
@@ -0,0 +1,20 @@
+import get_Discord_members
+import get_FC_members
+import CSV
+import csv
+
+def get_lists(source):
+    
+    member_list = []
+    if source == "Discord":
+        member_list = get_Discord_members.get_Discord_members()
+    if source == "XIV_API":
+        member_list = get_FC_members.get_FC_members()
+    filename = CSV.CSV_list(member_list, source)
+    return_value = []
+    with open(filename, newline='') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+        for row in csvreader:
+            return_value = row
+    return [item.strip(' ') for item in return_value]
+

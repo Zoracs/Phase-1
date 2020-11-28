09ec57d85ba37ccdaaed4e35c3b6d1315df8f006
Zoracs <regis24try@gmail.com>
2020-11-28 11:17:49 +0100

    added exception and tests for empty list and filename length and Null input

--- CSV.py
+++ CSV.py
@@ -6,8 +6,6 @@ def CSV_list(Username_list, Filename):
         raise ValueError("Your input was not a list.")
     if len(Filename) > 80:
         raise ValueError("Your Filename is too long, mate.")
-    if len(Username_list) == 0:
-        raise ValueError("Your list is empty.")
     File = open(f"{Filename}.csv", "w")
     lastEntry = len(Username_list) - 1
     for x in Username_list:
